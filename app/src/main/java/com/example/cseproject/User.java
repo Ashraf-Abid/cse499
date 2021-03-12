4a9cf73731aca7c7597301e944906f8056dc250a
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-03-12 18:57:08 +0600

    Added chat fragment

---
+++ app/src/main/java/com/example/cseproject/User.java
@@ -0,0 +1,14 @@
+package com.example.cseproject;
+
+public class User {
+    public String name,email,phone,type;
+
+    public User(){}
+
+    public User(String name, String email, String phone,String type) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.type=type;
+    }
+}
